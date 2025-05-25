49ca7b23d0f032cc0cab4298eb95834266d10b45
Gopal chandrashekhar Gawas <gopalgawas2004@gmail.com>
2025-05-25 15:53:55 +0530

    fix: secure MySQL connection with CA cert

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
-const mysql=require('mysql2');
-require('dotenv').config();
-const db = mysql.createConnection({
-  host:process.env.DB_HOST,
-  user:process.env.DB_USER,
-  password:process.env.DB_PASSWORD, 
-  database:process.env.DB_NAME,
-})
+const mysql = require("mysql2");
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
-db.connect((err)=>{
-  if(err){
-    console.error('Error connecting to the database:', err);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    // Load your CA certificate file
+    ca: fs.readFileSync("./ca.pem"), // make sure path to your CA file is correct
+  },
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
     return;
   }
-  console.log('Connected to the database');
-})
-module.exports = db;
+  console.log("Connected to the database!");
+});
--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/", schoolRoutes);
+// console.log({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
